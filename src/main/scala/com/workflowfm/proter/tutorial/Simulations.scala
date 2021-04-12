e28e7648b7ed9ea4d5d044d52b77e662886fd04c
Petros Papapanagiotou <petrospapapan@gmail.com>
2021-04-12 14:29:22 +0100

    Replaces random picks to use `Uniform` instead.

--- src/main/scala/com/workflowfm/proter/tutorial/Simulations.scala
+++ src/main/scala/com/workflowfm/proter/tutorial/Simulations.scala
@@ -4,20 +4,19 @@ import com.workflowfm.proter._
 import com.workflowfm.proter.flows._
 
 class PizzaOrderGenerator(waiters: Seq[TaskResource], chefs: Seq[TaskResource]) extends SimulationGenerator {
-  val r = scala.util.Random
-
-  def rand[T](s: Seq[T]): T = s(r.nextInt(s.size))
+  val rWaiter = Uniform(0, waiters.size)
+  val rChef = Uniform(0, chefs.size)
 
   override def build(manager: Manager, count: Int): Simulation = {
     val name = "Pizza " + count.toString()
 
-    val waiter: String = rand(waiters).name
-    val chef: String = rand(chefs).name
+    val waiter: String = waiters(rWaiter.getLong.toInt).name
+    val chef: String = chefs(rChef.getLong.toInt).name
 
-    val takeOrder: FlowTask = new FlowTask( Task("Take Order", Uniform(5, 10)) withResources(Seq(waiter)) )
-    val prepare: FlowTask = new FlowTask( Task("Prepare", Uniform(5, 10)) withResources(Seq(chef)) withCost 6 )
-    val bake: FlowTask = new FlowTask( Task("Bake", Uniform(20, 30)) withResources(Seq("Oven")) )
-    val serve: FlowTask = new FlowTask( Task("Serve", Uniform(1, 3)) withResources(Seq(waiter)) withPriority Task.High )
+    val takeOrder: FlowTask = Task("Take Order", Uniform(5, 10)) withResources(Seq(waiter))
+    val prepare: FlowTask = Task("Prepare", Uniform(5, 10)) withResources(Seq(chef)) withCost 6
+    val bake: FlowTask = Task("Bake", Uniform(20, 30)) withResources(Seq("Oven")) 
+    val serve: FlowTask = Task("Serve", Uniform(1, 3)) withResources(Seq(waiter)) withPriority Task.High 
 
     val flow: Flow = takeOrder > prepare > bake > serve
 
@@ -26,18 +25,16 @@ class PizzaOrderGenerator(waiters: Seq[TaskResource], chefs: Seq[TaskResource]) 
 }
 
 class GarlicBreadOrderGenerator(waiters: Seq[TaskResource]) extends SimulationGenerator {
-  val r = scala.util.Random
-
-  def rand[T](s: Seq[T]): T = s(r.nextInt(s.size))
+  val rWaiter = Uniform(0, waiters.size)
 
   override def build(manager: Manager, count: Int): Simulation = {
     val name = "Garlic Bread " + count.toString()
 
-    val waiter: String = rand(waiters).name
+    val waiter: String = waiters(rWaiter.getLong.toInt).name
 
-    val takeOrder: FlowTask = new FlowTask( Task("Take Order", Uniform(5, 10)) withResources(Seq(waiter)) )
-    val bake: FlowTask = new FlowTask( Task("Bake", Uniform(5, 10)) withResources(Seq("Oven")) )
-    val serve: FlowTask = new FlowTask( Task("Serve", Uniform(1, 3)) withResources(Seq(waiter)) withPriority Task.High )
+    val takeOrder: FlowTask = Task("Take Order", Uniform(5, 10)) withResources(Seq(waiter)) 
+    val bake: FlowTask = Task("Bake", Uniform(5, 10)) withResources(Seq("Oven")) 
+    val serve: FlowTask = Task("Serve", Uniform(1, 3)) withResources(Seq(waiter)) withPriority Task.High 
 
     val flow: Flow = takeOrder > bake > serve
 
